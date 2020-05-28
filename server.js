90573c1b1a2c4273bb1674d59d27712522c003fa
chethan.hanu <chethan.hanu@mfilterit.com>
2020-05-28 16:28:35 +0530

    Resolved validation errors

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -24,14 +24,40 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/timestamp/:date", (req, res) => {
-  var date = new Date(req.params.date);
-  var unixTimeStamp = Math.floor(date.getTime());
-  var utcDate = new Date(req.params.date).toUTCString();
-  res.json({"unix": unixTimeStamp, "utc": utcDate});
+app.get("/api/timestamp/:date_string", (req, res) => {
+  const dateString = req.params.date_string;
+  let date;
+  // If the date string is empty, it should be equivalent to new Date() to
+  // return the current time in unix format and UTC format
+  // i.e the service uses the current timestamp.
+  if (!dateString) {
+    date = new Date();
+  } else {
+        // if dateString is not empty
+        // if dateString is an integer, convert dateString to an integer
+    if (!isNaN(dateString)) {
+      date = new Date(parseInt(dateString));
+    } else {
+      date = new Date(dateString);
+    }
+    }
+  // If the date string is invalid the api returns and error JSON
+  // { "error": "Invalid Date" }
+  if (date.toString() === 'Invalid Date') {
+    res.json({ error: date.toString() });
+  } else {
+    // If the date string is valid the api returns a JSON like this format { "unix": <date.getTime()>, "utc" : <date.toUTCString()> }
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  }
 });
 
+app.get("/api/timestamp", (req, res) => {
+    let date = new Date();
+    res.json({"unix": Math.floor(date.getTime()), "utc": date.toUTCString()});
+});
+
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+let listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
